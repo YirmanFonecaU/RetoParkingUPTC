6f2fe022845c58be550a69a352c8ba02fcc05dfe
YirmanFonseca <fonsecasuarez03@gmail.com>
2025-03-31 21:47:32 -0500

    commitArray

--- src/co/edu/uptc/model/Ticket.java
+++ src/co/edu/uptc/model/Ticket.java
@@ -1,10 +1,12 @@
 package co.edu.uptc.model;
 
+import java.util.ArrayList;
+
 public class Ticket {
     private int ticketID;
     private Vehicle vehicle;
     private double totalPay;
-
+    private ArrayList<Object> registroTicket;
     public Ticket(){
         
     }

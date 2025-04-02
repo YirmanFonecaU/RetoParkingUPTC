26e2aee907a268207f86f4e4b8d82e68264fcbc8
YirmanFonseca <fonsecasuarez03@gmail.com>
2025-04-02 11:38:38 -0500

    commitGET&SETT

--- src/co/edu/uptc/model/Receptionist.java
+++ src/co/edu/uptc/model/Receptionist.java
@@ -7,6 +7,18 @@ public class Receptionist  extends User{
         super();
         
     }
+    public double getIncome() {
+        return income;
+    }
+    public void setIncome(double income) {
+        this.income = income;
+    }
+    public int getTotalVehiclesEntered() {
+        return totalVehiclesEntered;
+    }
+    public void setTotalVehiclesEntered(int totalVehiclesEntered) {
+        this.totalVehiclesEntered = totalVehiclesEntered;
+    }
     
 
 }
--- src/co/edu/uptc/model/User.java
+++ src/co/edu/uptc/model/User.java
@@ -1,15 +1,71 @@
 package co.edu.uptc.model;
 
+import java.util.Map;
+
 public class User {
-    private int id;
-    private String userName;
-    private String password;
-    private int phone;
-    private String direction;
-    private String email;
-    public User(){
+
+    protected int id;
+    protected String userName;
+    protected String password;
+    protected int phone;
+    protected String direction;
+    protected String email;
+    protected Map<String, String> account;
+
+    public User() {
 
     }
-    
+
+    public void logIn(String user, String password) {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }

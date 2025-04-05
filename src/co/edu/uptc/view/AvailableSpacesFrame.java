0ad37ebf3c18e51c4eaa6f9fe97d0ec45c255e5c
YirmanFonseca <fonsecasuarez03@gmail.com>
2025-04-04 22:25:28 -0500

    menuRecepcionista

---
+++ src/co/edu/uptc/view/AvailableSpacesFrame.java
@@ -0,0 +1,15 @@
+package co.edu.uptc.view;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class AvailableSpacesFrame extends JFrame {
+    public AvailableSpacesFrame(){
+        super("Availabe Spaces");
+        setSize(800,600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}

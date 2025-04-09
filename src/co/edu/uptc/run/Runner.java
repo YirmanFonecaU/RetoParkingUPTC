b4b2c61c087773bde846781bcc9089d0272d4fa3
LauraQuiroga7 <laura.quiroga04@uptc.edu.co>
2025-04-08 22:49:26 -0500

    Cambios
    cambios

--- src/co/edu/uptc/run/Runner.java
+++ src/co/edu/uptc/run/Runner.java
@@ -1,7 +1,8 @@
 package co.edu.uptc.run;
 
 import co.edu.uptc.presenter.Presenter;
-import co.edu.uptc.view.AdministrationMenuFrame;
+import co.edu.uptc.view.*;
+
 
 public class Runner {
     public static void main(String[] args) throws Exception {
